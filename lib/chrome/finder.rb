f98a04dbb4518e63ba75db9542dd62e81c0c0f9e
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-18 17:12:35 +0900

    wip: finder

---
+++ lib/chrome/finder.rb
@@ -0,0 +1,18 @@
+module Chrome
+  class Finder
+    def initialize(path)
+      @path = path || find
+    end
+
+    def command?
+      @path && File.executable?(@path)
+    end
+
+    def find
+      [ 'chromium' 'google-chrome' 'chromium-browser' ].find do |cmd|
+        p "cmd: #{cmd}"
+        false
+      end
+    end
+  end
+end
